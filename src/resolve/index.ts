7881ef9bc839596e378205376b721c98d1d77d60
Freeman H. <46896789+soranoo@users.noreply.github.com>
2026-08-01 15:51:42 +0800

    chore: fmt

--- src/resolve/index.ts
+++ src/resolve/index.ts
@@ -1,4 +1,4 @@
-export { IgnoreFilter } from "@/resolve/IgnoreFilter";
 export type { IResolver } from "@/resolve/Resolver";
+export { IgnoreFilter } from "@/resolve/IgnoreFilter";
 export { OxcResolver } from "@/resolve/OxcResolver";
 export { VirtualAwareResolver } from "@/resolve/VirtualAwareResolver";
